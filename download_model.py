8fdde8c3dc9b7686869e6458d6005c1117eb77d8
Gordon <gkamer@outlook.com>
2022-05-30 21:22:59 -0400

    model download complete

---
+++ download_model.py
@@ -0,0 +1,16 @@
+from transformers import AutoModel
+import os
+
+# Note: list available at https://huggingface.co/models
+# Cached models are by default stored in ~/.cache/huggingface/transformers/
+
+def download_model(model_hf_path, download_folder):
+    model = AutoModel.from_pretrained(model_hf_path)
+    model.save_pretrained(download_folder)
+
+if __name__ == '__main__':
+    MODEL_TO_DOWNLOAD = 'distilgpt2'
+    MODEL_DOWNLOAD_FOLDER = 'model-downloads'
+    download_folder = os.path.join(MODEL_DOWNLOAD_FOLDER, MODEL_TO_DOWNLOAD)
+    download_model(MODEL_TO_DOWNLOAD, download_folder)
+
